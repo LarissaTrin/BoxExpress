d17785da93cfba9461bd36706c1c6f65c959a8cd
Lua <lala.trindade.2008@gmail.com>
2022-09-06 01:26:55 -0300

    add navbar

--- boxExpress/src/app/app.module.ts
+++ boxExpress/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
