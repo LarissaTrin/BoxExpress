b9483448192265cb7fe7d09f09bf8313ea0c9e0a
Larissa Trindade de Araujo Tredinnick <larissa.trindade@radixeng.com.br>
2022-09-10 00:44:29 -0300

    forms configurado

--- src/app/envios/envios.component.ts
+++ src/app/envios/envios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-envios',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnviosComponent implements OnInit {
 
-  constructor() { }
+  public pedidoForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.criarForm();
+  }
 
   ngOnInit(): void {
   }
 
+  criarForm () {
+    this.pedidoForm = this.fb.group({
+      id: [''],
+      nomeRemetente: ['', Validators.required],
+      enderecoRemetente: ['', Validators.required],
+      nomeDestino: ['', Validators.required],
+      enderecoDestino: ['', Validators.required],
+      pacotes: ['Quantidade de Pacotes', Validators.required],
+      peso: ['Peso Total da Carga...', Validators.required],
+      isCaixas: [false],
+    });
+  }
+
+  pedidoSubmit() {
+    console.log(this.pedidoForm?.value);
+  }
 }
