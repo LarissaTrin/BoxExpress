8de9791916c9acdf8f7e28b4259276e5eb3d71e6
Lua <lala.trindade.2008@gmail.com>
2022-09-14 23:30:49 -0300

    tela pedidos vinculada

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pedidos } from '../models/Pedidos';
 import { DataService } from '../Service/service.service';
 
 @Component({
@@ -10,13 +11,29 @@ export class PedidosComponent implements OnInit {
 
   clientLogin: boolean | undefined;
   deliveryLogin: boolean | undefined;
+  pedidos_service: any;
+  pedidosList: Pedidos[] = [];
+  numPedidos: number = 0;
+
   constructor(
     private data: DataService,
-  ) { }
+  ) {  }
 
   ngOnInit(): void {
     this.data.currentClientLogin.subscribe(clientLogin => this.clientLogin = clientLogin);
     this.data.currentDeliveryLogin.subscribe(deliveryLogin => this.deliveryLogin = deliveryLogin);
+    this.data.currentPedidos.subscribe(pedidos => {
+      this.numPedidos = pedidos.length - 1;
+      this.pedidos_service = pedidos
+    });
+
+    this.criaLista();
   }
 
+  criaLista() {
+    for (let i = 1; i < this.pedidos_service.length; i++) {
+      this.pedidosList.push(this.pedidos_service[i]);
+    }
+    console.log("pedidosList: ", this.pedidosList)
+  }
 }
